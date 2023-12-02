f56c528bbe5b2ab67ecdbdf73ffcd80010dc0c0a
DreamShark-Bytes <DreamShark.Chomp@gmail.com>
2023-12-02 09:44:21 -0600

    mid-way thu day 2

---
+++ 2023/Day02-Cube Conundrum/day02.py
@@ -0,0 +1,89 @@
+import os
+import logging
+import time
+import re
+
+file_path = os.path.dirname(os.path.abspath(__file__)) + os.sep
+input_file = file_path + 'day02_input.txt'
+padding_size_large = 100
+padding_size_medium = 45
+padding_char = '-'
+
+class cube:
+	def __init__(self,color,limit,count=0):
+		self.color = color
+		self.limit = limit
+		self.count = count
+
+def prb(prb_num,lines_to_debug = 0):
+	
+	limits = [
+		cube('red', 12),
+		cube('green', 13),
+		cube('blue', 14),
+	]
+	limits = {
+		'red':12,
+		'green':13,
+		'blue':14,
+	}
+	passing_game_id_count = 0
+	total_power = 0
+	
+	with open(input_file,'r') as f:
+		lines = f.read().split('\n')
+
+	lines_read = 0
+	for game_played in lines:
+		lines_read += 1
+		
+		pull_results = []
+		# cleaned_line = re.sub(r'[^a-zA-Z :;0-9]', '', line)
+		max_colors = {key:0 for key in limits.keys()}
+		game_id = int(game_played.split(':')[0][5:])
+		pulls = game_played.split(':')[1].split(';')
+		id_pass = True
+		
+		for pull in pulls:
+			pull_results += [True]
+			pull = [p.strip() for p in pull.split(',')]
+			for view in pull:
+				view = view.split()
+				amount = int(view[0])
+				color = view[1]
+				if amount > max_colors[color]:
+					max_colors[color] = amount
+				if amount > limits[color]:
+					id_pass=False
+					pull_results[-1] = False
+			if not id_pass: break
+		game_power = 0
+		if id_pass:
+			passing_game_id_count +=  game_id
+			game_power = 1
+			for value in max_colors.values(): game_power *= value
+			total_power += game_power
+
+		if lines_read <= lines_to_debug:
+			logging.debug(f'{game_played}')
+			logging.debug('\t' + str(pull_results))
+			if id_pass:
+				logging.debug('\t' + 'PASS::')
+				logging.debug('\t\t' + f'{passing_game_id_count=}')
+				logging.debug('\t\t' + f'{max_colors=}')
+				logging.debug('\t\t' + f'{game_power=} :: {total_power=}')
+	
+	if prb_num == 1:
+		logging.info(f'ANSWER: {passing_game_id_count=}')
+	elif prb_num == 2:
+		logging.info(f'ANSWER: {total_power=}')
+
+def handler():
+	logging.getLogger().setLevel(logging.DEBUG)
+	logging.info(' day 2, problem 1 '.center(padding_size_large,padding_char))
+	prb(1) # Answer: 2331
+
+	logging.info(' day 2, problem 2 '.center(padding_size_large,padding_char))
+	prb(2,20)
+
+handler()
---
+++ html_to_markdown.py
@@ -0,0 +1,99 @@
+'''
+TODO: 
+- keep "comment" section from existing README.md
+- try getting active user session using COOKIES
+- read first <article> as Title<h1>, and manually add a "Problem 1" <h2>
+    - strip '-' and ' '
+-------------------------------------
+To Run: 
+    - Get HTML of Problem's Page, regardless of browser the steps 
+    for this should remain relatively the same
+        - right click page
+        - choose Inspect
+        - right click top most element that begins with "<html"
+        - copy > copy element
+    - paste the contents into the file: "html.txt"
+'''
+
+import os
+from bs4 import BeautifulSoup # pip install beautifulsoup4
+from markdownify import markdownify
+import glob
+import logging
+import re
+
+# Configuration ------------------------------------------------------
+year = 2023
+day = '1'
+
+file_path = os.path.dirname(os.path.abspath(__file__)) + os.sep
+input_file_path = file_path + 'html.txt'
+output_file = 'README_temp.md'
+
+# Input ------------------------------------------------------------
+with open(input_file_path) as f:
+    html = f.read()
+    
+soup = BeautifulSoup(html, "html.parser")
+
+h2 = soup.find('h2')
+day_title = ''
+print('Reading HTML')
+if h2:
+    day = re.findall(r'\d+', h2.text)[0]
+    s = h2.text.split(':')[1]
+    day_title = ''.join([c for c in s if c.isalpha() or c==' ']).strip()
+    print('\t-' + f'Day found from HTML: {day}')
+day_correct = not input('Is the day found correct? (Leave empty for YES)')
+if not day_correct:
+    quit()
+
+# Does the Folder already exist -----------------------------------------
+day = day.zfill(2)
+output_folder = glob.glob(file_path + str(year) + os.sep + 'Day' + day + '*')
+if output_folder: 
+    output_folder = output_folder[0]
+else:
+    print('Could not find folder for output')
+    new_folder = "Day" + day + "-" + day_title
+    print(f'Would you like to create the folder? -- "{new_folder}"')
+    create_folder = not input('Leave empty for YES ')
+    if create_folder:
+        output_folder = file_path + str(year) + os.sep + new_folder
+        os.mkdir(output_folder)
+        with open (output_folder + os.sep + 'day' + day + '.py','w') as f: pass
+        with open (output_folder + os.sep + 'day' + day + '_input.txt','w') as f: pass
+    else: 
+        quit()
+
+# Does the Markdown file exist ----------------------------------------
+output_file_path = output_folder + os.sep + output_file
+print(output_file_path)
+comments = '\n\n## Comments / Notes\n'
+if os.path.isfile(output_file_path):
+    with open(output_file_path,'r') as f:
+        lines = f.read()
+        print(f'{type(lines)=}')
+
+# Remove Answers and non-needed info -------------------------------
+output = ''
+arts = soup.find_all('article')
+for art in arts:
+    for elem in art.find_all('p'):
+        if elem.find(text=re.compile(f'Your puzzle answer .*')):
+            # print(elem)
+            elem.decompose()
+            # Now find all following elements and remove them
+            for sibling in elem.find_next_siblings():
+                # if sibling.name == "article":
+                #    break
+                # print(f'\t-{sibling}')
+                sibling.decompose()
+    output += markdownify(str(art), heading_style="ATX")
+output += comments
+
+quit()
+
+# Output ------------------------------------------------------------
+with open (output_file_path,'w') as f:
+    f.write(md)
